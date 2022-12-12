03bce0bcab1a53042d1cd69450503cb6cb243158
Jason Yarrington <jason@fastforward.sh>
2022-12-12 10:51:09 -0500

    Monkies in progress

--- day11/day11.test.js
+++ day11/day11.test.js
@@ -1,17 +1,43 @@
 
 const testFileName = "./day11/test.txt"
 const dataFileName = "./day11/data.txt"
-const run = require('./day11.js')
+const { run, Monkey, Monkies } = require('./day11.js')
 
 
 test('Test', () => {
-    const answer = run(testFileName)
-    expect (answer.hello).toBe("Hello world")
+
+const m = 
+`Monkey 0:
+Starting items: 79, 98
+Operation: new = old * 19
+Test: divisible by 23
+  If true: throw to monkey 2
+  If false: throw to monkey 3`
+
+  let monkey = new Monkey(m)
+
+    expect(monkey.id).toBe(0)
+    expect(monkey.items).toStrictEqual([79, 98])
+    expect(monkey.operation).toBe('new = old * 19')
+    expect(monkey.test).toBe('divisible by 23')
+    expect(monkey.ifTrue).toBe(2)
+    expect(monkey.ifFalse).toBe(3)
+
+    const monkies = new Monkies(m)
+
+    monkey = monkies.getMonkey(0)
+
+    expect(monkey.id).toBe(0)
+    expect(monkey.items).toStrictEqual([79, 98])
+    expect(monkey.operation).toBe('new = old * 19')
+    expect(monkey.test).toBe('divisible by 23')
+    expect(monkey.ifTrue).toBe(2)
+    expect(monkey.ifFalse).toBe(3)
 })
 
-test('Answer', () => {
+// test('Answer', () => {
     
-    const answerData = run(dataFileName)
-    expect (answerData.hello).toBe("Hello world")
+//     const answerData = run(dataFileName)
+//     expect (answerData.hello).toBe("Hello world")
 
-})
+// })
